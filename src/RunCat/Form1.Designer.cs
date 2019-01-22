1866f10e97bc51dfb6878b45ffedb020ceabdf67
old.man <sunth0101@126.com>
2019-01-22 23:50:19 +0800

    first commit

---
+++ src/RunCat/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace RunCat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 18);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(64, 45);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Black;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ src/RunCat/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunCat
+{
+    public partial class Form1 : Form
+    {
+        private List<Image> _image;
+        private int _currentImageIndex = 0;
+        private int _runerTimerIntervalMax = 500;
+        private readonly Timer _runerTimer;
+        private readonly Timer _cpuCounterRefreshTimer;
+        private PerformanceCounter _cpuCounter;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _cpuCounterRefreshTimer = new Timer();
+            _cpuCounterRefreshTimer.Interval = 1000;
+            _cpuCounterRefreshTimer.Tick += _cpuCounterRefreshTimer_Tick;
+
+            _runerTimer = new Timer();
+            _runerTimer.Tick += RunerTimerTick;
+
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _image = LoadResouces("cat.cat_page", 5);
+
+            _runerTimer.Interval = _runerTimerIntervalMax;
+            _runerTimer.Start();
+
+            _cpuCounterRefreshTimer.Start();
+        }
+
+        private void _cpuCounterRefreshTimer_Tick(object sender, EventArgs e)
+        {
+            var cpuUseage = GetCpuUseage();
+            _runerTimer.Interval = ComputeTimerInterval(cpuUseage);
+        }
+
+        private void RunerTimerTick(object sender, EventArgs e)
+        {
+            SetImageSource(_image[_currentImageIndex]);
+
+            if (_currentImageIndex == _image.Count - 1)
+            {
+                _currentImageIndex = 0;
+            }
+            else
+            {
+                _currentImageIndex++;
+            }
+        }
+
+        private void SetImageSource(Image image)
+        {
+            if (image == null)
+            {
+                return;
+            }
+
+            pictureBox1.Invoke(new Action(() => { pictureBox1.Image = image; }));
+        }
+
+        private List<Image> LoadResouces(string prefix,int imageCount)
+        {
+            var resources = new List<Image>();
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            for (int i = 0; i < imageCount; i++)
+            {
+                var image = Image.FromStream(assembly.GetManifestResourceStream($"RunCat.images.{prefix}{i}.png"));
+                resources.Add(image);
+            }
+
+            return resources;
+        }
+
+        private float GetCpuUseage()
+        {
+            if (_cpuCounter == null)
+            {
+                _cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            }
+
+            return _cpuCounter.NextValue();
+        }
+
+        private int ComputeTimerInterval(float cpuUseage)
+        {
+            if (cpuUseage < 1)
+            {
+                return _runerTimerIntervalMax;
+            }
+
+            var interval = cpuUseage / 100 * _runerTimerIntervalMax;
+            return (int) interval;
+        }
+    }
+}
